500a531e612021e8d2dc1bd4a7b781007ccf7e6d
Florian Dagner <99618839+fdagner@users.noreply.github.com>
2024-01-07 00:35:45 +0100

    update3

--- js/filter.js
+++ js/filter.js
@@ -1,108 +1,141 @@
 // Function for filtering the options based on the search field
-function sucheOrt() {
-  var input = document.getElementById("suchfeld").value.toLowerCase();
-  var options = document.getElementById("ortAuswahl").options;
+function myFunction()
+{
+    document.getElementById("ortAuswahl").classList.toggle("show");
+}
 
-  var visibleOptionsCount = 0;
+function filterFunction()
+{
+    var input, filter, div, options, i;
+    input = document.getElementById("searchInput");
+    filter = input.value.toUpperCase();
+    div = document.getElementById("ortAuswahl");
+    options = div.getElementsByTagName("a");
 
-  for (var i = 0; i < options.length; i++) {
-    var optionText = options[i].text.toLowerCase();
-    var isVisible = optionText.includes(input);
-    options[i].style.display = isVisible ? "block" : "none";
-
-    if (isVisible) {
-      visibleOptionsCount++;
+    for (i = 0; i < options.length; i++)
+    {
+        txtValue = options[i].textContent || options[i].innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1)
+        {
+            options[i].style.display = "";
+        }
+        else
+        {
+            options[i].style.display = "none";
+        }
     }
-  }
+}
 
-  // Dynamically change the size of the drop-down menu
-  var dropdown = document.getElementById("ortAuswahl");
-  dropdown.size = Math.min(visibleOptionsCount, 10); // Hier können Sie die maximale Anzahl der sichtbaren Optionen anpassen (z. B. 10).
+function toggleSelection(element)
+{
+    // Alle anderen ausgewählten Elemente entfernen
+    let allSelectedElements = document.querySelectorAll("#ortAuswahl a.selected");
+    allSelectedElements.forEach(selectedElement =>
+    {
+        if (selectedElement !== element)
+        {
+            selectedElement.classList.remove("selected");
+        }
+    });
 
-// Reset the size to 1 if only one result was found
-  if (visibleOptionsCount === 1) {
-    dropdown.size = 2;
-  }
+    // Die "selected"-Klasse zum aktuellen ausgewählten Element hinzufügen
+    element.classList.toggle("selected");
 }
 
 // Function for showing and hiding the location based on the checkbox status
-function toggleOrtVisibility() {
-  const showOrtCheckbox = document.getElementById("showOrtCheckbox");
-  const isOrtVisible = showOrtCheckbox.checked;
+function toggleOrtVisibility()
+{
+    const showOrtCheckbox = document.getElementById("showOrtCheckbox");
+    const isOrtVisible = showOrtCheckbox.checked;
 
-  if (isOrtVisible) {
-    myChart.options.plugins.title.display = true;
-  } else {
-    myChart.options.plugins.title.display = false;
-  }
-  myChart.update();
+    if (isOrtVisible)
+    {
+        myChart.options.plugins.title.display = true;
+    }
+    else
+    {
+        myChart.options.plugins.title.display = false;
+    }
+    myChart.update();
 }
 
-function toggleSubtitleVisibility() {
-  const showSubtitleCheckbox = document.getElementById("showSubtitleCheckbox");
-  const isSubtitleVisible = showSubtitleCheckbox.checked;
+function toggleSubtitleVisibility()
+{
+    const showSubtitleCheckbox = document.getElementById("showSubtitleCheckbox");
+    const isSubtitleVisible = showSubtitleCheckbox.checked;
 
-  if (isSubtitleVisible) {
-    myChart.options.plugins.subtitle.display = true;
-  } else {
-    myChart.options.plugins.subtitle.display = false;
-  }
-  myChart.update();
+    if (isSubtitleVisible)
+    {
+        myChart.options.plugins.subtitle.display = true;
+    }
+    else
+    {
+        myChart.options.plugins.subtitle.display = false;
+    }
+    myChart.update();
 }
 
-function togglePrecipitationVisibility() {
-  const showPrecipitationCheckbox = document.getElementById(
-    "showPrecipitationCheckbox"
-  );
-  const isPrecipitationVisible = showPrecipitationCheckbox.checked;
+function togglePrecipitationVisibility()
+{
+    const showPrecipitationCheckbox = document.getElementById(
+        "showPrecipitationCheckbox"
+    );
+    const isPrecipitationVisible = showPrecipitationCheckbox.checked;
 
-  const precipitationDataset1 = myChart.data.datasets[1];
-  const precipitationDataset2 = myChart.data.datasets[2];
-  const precipitationDataset3 = myChart.data.datasets[4];
-  const precipitationDataset4 = myChart.data.datasets[5];
+    const precipitationDataset1 = myChart.data.datasets[1];
+    const precipitationDataset2 = myChart.data.datasets[2];
+    const precipitationDataset3 = myChart.data.datasets[4];
+    const precipitationDataset4 = myChart.data.datasets[5];
 
-  if (isPrecipitationVisible) {
-    precipitationDataset1.fill = {
-      target: "-1",
-      above: "rgba(0,176,240,0.75)",
-    };
-    precipitationDataset2.borderWidth = 3;
-    precipitationDataset2.fill = {
-      target: "-1",
-      above: "rgba(64,105,156,0.75)",
-    };
-    precipitationDataset3.backgroundColor = "#00B0F0";
-    precipitationDataset4.backgroundColor = "#40699C";
-  } else {
-    precipitationDataset1.fill = false;
-    precipitationDataset2.borderWidth = 0;
-    precipitationDataset2.fill = false;
-    precipitationDataset3.backgroundColor = "transparent";
-    precipitationDataset4.backgroundColor = "transparent";
-  }
-  myChart.update();
+    if (isPrecipitationVisible)
+    {
+        precipitationDataset1.fill = {
+            target: "-1",
+            above: "rgba(0,176,240,0.75)",
+        };
+        precipitationDataset2.borderWidth = 3;
+        precipitationDataset2.fill = {
+            target: "-1",
+            above: "rgba(64,105,156,0.75)",
+        };
+        precipitationDataset3.backgroundColor = "#00B0F0";
+        precipitationDataset4.backgroundColor = "#40699C";
+    }
+    else
+    {
+        precipitationDataset1.fill = false;
+        precipitationDataset2.borderWidth = 0;
+        precipitationDataset2.fill = false;
+        precipitationDataset3.backgroundColor = "transparent";
+        precipitationDataset4.backgroundColor = "transparent";
+    }
+    myChart.update();
 }
 
-function toggleTemperatureVisibility() {
-  const showTemperatureCheckbox = document.getElementById(
-    "showTemperatureCheckbox"
-  );
-  const isTemperatureVisible = showTemperatureCheckbox.checked;
+function toggleTemperatureVisibility()
+{
+    const showTemperatureCheckbox = document.getElementById(
+        "showTemperatureCheckbox"
+    );
+    const isTemperatureVisible = showTemperatureCheckbox.checked;
 
-  const temperatureDataset1 = myChart.data.datasets[0];
-  const temperatureDataset2 = myChart.data.datasets[3];
+    const temperatureDataset1 = myChart.data.datasets[0];
+    const temperatureDataset2 = myChart.data.datasets[3];
 
-  if (isTemperatureVisible) {
-    temperatureDataset1.borderWidth = 3;
-    temperatureDataset2.borderWidth = 3;
-    temperatureDataset1.fill = {
-      target: "1",
-      above: "rgba(255, 252, 127, 0.75)",
-    };
-  } else {
-    temperatureDataset1.borderWidth = 0;
-    temperatureDataset2.borderWidth = 0;
-    temperatureDataset1.fill = false;
-  }
-  myChart.update();
+    if (isTemperatureVisible)
+    {
+        temperatureDataset1.borderWidth = 3;
+        temperatureDataset2.borderWidth = 3;
+        temperatureDataset1.fill = {
+            target: "1",
+            above: "rgba(255, 252, 127, 0.75)",
+        };
+    }
+    else
+    {
+        temperatureDataset1.borderWidth = 0;
+        temperatureDataset2.borderWidth = 0;
+        temperatureDataset1.fill = false;
+    }
+    myChart.update();
 }
--- js/map.js
+++ js/map.js
@@ -1,42 +1,54 @@
- // Coordinates for the marker
- const markerCoordinates = [parseFloat(latData), parseFloat(lonData)]; // Koordinaten in Zahlen umwandeln
+// Coordinates for the marker
+const markerCoordinates = [parseFloat(latData), parseFloat(lonData)]; // Koordinaten in Zahlen umwandeln
 
- // Create the leaflet card
- const myMap = L.map("map",{
-minZoom: 1,
-maxZoom: 4,
+// Create the leaflet card
+const myMap = L.map("map",
+{
+    minZoom: 1,
+    maxZoom: 4,
 }).setView(markerCoordinates, 1);
 
- // Add the OpenStreetMap basemap
- L.tileLayer("js/map/{z}/{x}/{y}.png", {
-attribution: 'Kartendaten: © <a href="https://openstreetmap.org/copyright">OpenStreetMap</a>-Mitwirkende, SRTM | Kartendarstellung: <a href="https://opentopomap.org/">© OpenTopoMap</a> <a href="https://creativecommons.org/licenses/by-sa/3.0/">(CC-BY-SA)</a>',
+// Add the OpenStreetMap basemap
+L.tileLayer("js/map/{z}/{x}/{y}.png",
+{
+    attribution: 'Kartendaten: © <a href="https://openstreetmap.org/copyright">OpenStreetMap</a>-Mitwirkende, SRTM | Kartendarstellung: <a href="https://opentopomap.org/">© OpenTopoMap</a> <a href="https://creativecommons.org/licenses/by-sa/3.0/">(CC-BY-SA)</a>',
 }).addTo(myMap);
 
 // Add a marker with a user-defined icon
- const customIcon = L.icon({
-   iconUrl: 'img/ts-map-pin.svg',
-   iconSize: [32, 32], 
-   iconAnchor: [16, 28], //Position of the anchor relative to the icon position
- });
+const customIcon = L.icon(
+{
+    iconUrl: 'img/ts-map-pin.svg',
+    iconSize: [32, 32],
+    iconAnchor: [16, 28], //Position of the anchor relative to the icon position
+});
 
 // Add the marker
- L.marker(markerCoordinates, { icon: customIcon }).addTo(myMap);
+L.marker(markerCoordinates,
+{
+    icon: customIcon
+}).addTo(myMap);
 
 // Function for updating the leaflet map based on the new coordinates
- function updateLeafletMap() {
- // New coordinates
-   const newLatLng = L.latLng(parseFloat(latData), parseFloat(lonData));
+function updateLeafletMap()
+{
+    // New coordinates
+    const newLatLng = L.latLng(parseFloat(latData), parseFloat(lonData));
 
-   // Remove existing markers to avoid duplicate markers
-   myMap.eachLayer((layer) => {
-     if (layer instanceof L.Marker) {
-       layer.remove();
-     }
-   });
+    // Remove existing markers to avoid duplicate markers
+    myMap.eachLayer((layer) =>
+    {
+        if (layer instanceof L.Marker)
+        {
+            layer.remove();
+        }
+    });
 
-   // Add the new marker
-   L.marker(newLatLng, { icon: customIcon }).addTo(myMap);
+    // Add the new marker
+    L.marker(newLatLng,
+    {
+        icon: customIcon
+    }).addTo(myMap);
 
-   // Update the map
-   myMap.setView(newLatLng, 1);
- }
+    // Update the map
+    myMap.setView(newLatLng, 1);
+}
